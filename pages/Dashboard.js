9404a2729d4ee8a10d8d6f18fe4618c168b2acbc
SK Hasibul Hossain <86801974+sk-hasibul-hossain@users.noreply.github.com>
2023-12-17 10:23:24 +0530

    Update Dashboard.js

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -3,15 +3,21 @@ import { AuthContext } from "./_app";
 
 
 function Dashboard() {
-  const { btn1, btn2, setbtn1, setbtn2 } = useContext(AuthContext);
+  const { button1, button2 } = useContext(AuthContext);
+  
 
   return (
     <div id="dashboardPage">
-     <button id="btn1" onClick={() => {setbtn1((prev) => !prev)}}>Button 1</button>
-      <button id="btn2" onClick={() => {setbtn2((prev) => !prev)}}>Button 2</button>
-      {btn1 && btn2 ? <div className="msg">Both are true</div>
-      : !btn1 || !btn2 ? <div className="msg">One on them is false</div>
-      : <div className="msg">Both are false</div>}
+      <button id="btn1" onClick={()=>{
+        button1.setBtn1(n=>!n)
+      }}>Button 1</button>
+      <button id="btn2" onClick={()=>{
+        button2.setBtn2(n=>!n)
+      }}>Button 2</button>
+      {button1.btn1 && button2.btn2 && <div className="msg">Both are true</div>}
+      {((!button1.btn1 && button2.btn2) ||  (button1.btn1 && !button2.btn2)) &&<div className="msg">One on them is false</div>}
+      {(!button1.btn1 && !button2.btn2) &&<div className="msg">Both are false</div>}
+
     </div>
   );
 }
